a8b220bf02711a28fcd06f74213dfb6966986b33
Zachary Scott <zachary.s.scott@gmail.com>
2010-06-20 20:49:05 -0400

    adding delete task js

--- public/javascript/tewdew.js
+++ public/javascript/tewdew.js
@@ -1,10 +1,20 @@
 $(document).ready(function() {
-  $(".button").click(function(){
+  $(".addTask").click(function(){
     var taskTitle = $(".taskTitle").val();
     $.post("/add", {title: taskTitle}, function() {
       $.get("/tasks", function(data) {
         $("#tasks").html(data);
       });
+    });
+    return false;
+  });
+  
+  $(".deleteTask").live(function(){
+    var method = "delete";
+    var task_id = $(".task_id").val();
+    var taskelem = ".task-" + task_id;
+    $.post("/delete/" + task_id, function(data) {
+      $(taskelem).fadeOut('slow', function(){});
     });
     return false;
   });
