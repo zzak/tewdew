782e8572cd3cf96eb1c517e1d7adbdb46ed3f8b2
Zachary Scott <zachary.s.scott@gmail.com>
2010-08-02 21:59:43 -0400

    using sinatra_warden for authorization

--- main.rb
+++ main.rb
@@ -1,32 +1,26 @@
 require 'rubygems'
-require 'sinatra'
+require 'sinatra/base'
 require 'haml'
 require 'sass'
 require 'dm-core'
 require 'dm-migrations'
 require 'dm-validations'
-require 'twitter_oauth'
+require 'sinatra_warden'
 
 configure :test do 
   puts 'Test configuration in use'
   DataMapper.setup(:default, 'sqlite::memory:')
-  set :sessions, true
-  @@config = YAML.load_file("config.yml") rescue nil || {}
 end 
 
 configure :development do 
   puts 'Development configuration in use'
   DataMapper::Logger.new($stdout, :debug)
   DataMapper.setup(:default, "sqlite://#{Dir.pwd}/db/development.db")
-  set :sessions, true
-  @@config = YAML.load_file("config.yml") rescue nil || {}
 end
 
 configure :production do
   # production mode
   DataMapper.setup(:default, ENV['DATABASE_URL'] || 'postgres://my.db')
-  set :sessions, true
-  @@config = YAML.load_file("config.yml") rescue nil || {}
 end
 
 class Task
@@ -51,16 +45,7 @@ end
 DataMapper.finalize.auto_upgrade!
 
 class Application < Sinatra::Base
-  before do
-    @user = session[:user]
-    @client = TwitterOAuth::Client.new(
-      :consumer_key => @@config['consumer_key'],
-      :consumer_secret => @@config['consumer_secret'],
-      :token => session[:access_token],
-      :secret => session[:secret_token]
-    )
-    @rate_limit_status = @client.rate_limit_status
-  end
+  register Sinatra::Warden
 
   helpers do
     # Usage: partial :foo
@@ -117,39 +102,6 @@ class Application < Sinatra::Base
     @task = Task.get(params[:task_id])
     @task.destroy!
     redirect "/"
-  end
-  
-  get '/connect' do
-    request_token = @client.request_token
-    session[:request_token] = request_token.token
-    session[:request_token_secret] = request_token.secret
-    redirect request_token.authorize_url  
-  end
-
-  get '/auth' do
-    # Exchange the request token for an access token.
-    @access_token = @client.authorize(
-      session[:request_token],
-      session[:request_token_secret]
-    )
-    
-    if @client.authorized?
-        session[:access_token] = @access_token.token
-        session[:secret_token] = @access_token.secret
-        session[:user] = true
-        redirect '/tasks'
-      else
-        redirect '/'
-    end
-  end
-
-  get '/disconnect' do
-    session[:user] = nil
-    session[:request_token] = nil
-    session[:request_token_secret] = nil
-    session[:access_token] = nil
-    session[:secret_token] = nil
-    redirect '/'
   end
 end
 
