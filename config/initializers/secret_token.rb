29a43e45677a0cc0b614f26ec601dc1d66ba1f85
Zachary Scott <zachary.s.scott@gmail.com>
2010-08-04 02:37:50 -0400

    adding a rails3 branch to start hacking there too

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tewdew::Application.config.secret_token = '546c379f0f1403ec27240ca7b8b0d4222a2537399ed2b4e23325ed26a9c8a97c35d82b4664abe87b2cee4b7b15686127d8946a9519a03eb80b62e26309c5354d'
